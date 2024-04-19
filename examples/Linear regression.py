bae9802adad6d6d46cb95024c15a02bf1a552e78
mavritsa <amavrits@gmail.com>
2024-04-19 20:10:55 +0200

    Changed while_loop to scan

---
+++ examples/Linear regression.py
@@ -0,0 +1,73 @@
+from src.main import GradientDescent
+import numpy as np
+import jax
+import jax.numpy as jnp
+import optax
+from scipy import stats
+from time import time
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    np.random.seed(42)
+    x = np.random.normal(size=100)
+    np.random.seed(32)
+    y = 3 + 2 * x + np.random.normal(size=100)
+    data = (np.c_[np.ones_like(x), x], y)
+
+    ols_slope, ols_intercept, _, stderr, intercept_stderr = stats.linregress(x, y)
+
+    def objective_fn(theta, data):
+        yhat = data[0].dot(theta)
+        y = data[1]
+        loss = jnp.sum((y - yhat)**2) / y.size
+        return loss
+
+    def initilization_fn(rng):
+        return jax.random.normal(rng, shape=(2, ))
+
+    scheduler = optax.exponential_decay(init_value=1e-3, transition_steps=1000, decay_rate=0.99)
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(1.0),  # Clip by the gradient by the global norm.
+        optax.scale_by_adam(),  # Use the updates from adam.
+        optax.scale_by_schedule(scheduler),  # Use the learning rate from the scheduler.
+        optax.scale(-1.0) # Scale updates by -1 since optax.apply_updates is additive and we want to descend on the loss.
+    )
+
+    rng = jax.random.PRNGKey(42)
+    rng_fit, rng_boot = jax.random.split(rng, 2)
+    gd = GradientDescent(objective_fn, initilization_fn, data, optimizer, obj_threshold=1e-3, grad_threshold=1e-3, max_epochs=2_000)
+    start_time = time()
+    res, metrics = gd._run(rng_fit, n_inits=10)
+    res_boot, metrics_boot = gd._bootstrap(rng_boot, n_inits=1, n_boot=1_000)
+    end_time = time()
+    print(end_time - start_time)
+
+
+    fig, axs = plt.subplots(1, 2)
+
+    axs[0].scatter(res_boot.theta.mean(axis=0), np.arange(2), color="b", label="GD Bootstrap mean")
+    axs[0].scatter([ols_intercept, ols_slope], np.arange(2)+0.2, color="b", label="Analytical mean")
+
+    axs[0].scatter(res.theta, np.arange(2), marker="x", color="r", label="GD OLS")
+    axs[0].scatter([ols_intercept, ols_slope], np.arange(2)+0.2, marker="x", color="r", label="Analytical OLS")
+
+    axs[0].set_xlabel("Coefficient value", fontsize=12)
+    axs[0].set_ylabel("Coefficient", fontsize=12)
+    axs[0].legend(fontsize=10)
+
+    axs[1].plot(metrics["epoch"]+1, metrics["objective_value"], color="b", label="Fit")
+    axs[1].plot(metrics["epoch"]+1,metrics_boot["objective_value"].mean(axis=0), linestyle="--", color="b", label="Bootstrap mean")
+    axs[1].fill_between(metrics["epoch"]+1,
+                        np.quantile(metrics_boot["objective_value"], 0.05, axis=0),
+                        np.quantile(metrics_boot["objective_value"], 0.95, axis=0),
+                        color="b", alpha=0.3, label="Bootstrap 90% CI")
+    axs[1].set_xlabel("Epoch", fontsize=12)
+    axs[1].set_ylabel("Objective value", fontsize=12)
+    axs[1].legend(fontsize=10)
+
+
+
--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 from jax import lax
-import optax
 from jax_tqdm import scan_tqdm, loop_tqdm
 from flax.training.train_state import TrainState
 from dataclasses import dataclass
@@ -11,83 +10,143 @@ from functools import partial
 class TrainState(TrainState):
     obj_keeper: jnp.float32
     grads_keeper: jnp.array
-    max_epochs: jnp.int32
     converged: jnp.bool_
+    convergence_epoch: jnp.int32
 
 @dataclass
-class GD_Output:
+class FitResults:
     theta: np.array
     converged: bool
-    step: int
+    convergence_epoch: int
     objective_value: float
     grads: np.array
 
 class GradientDescent:
 
-    def __init__(self, objective_fn, initilization_fn, data, optimizer):
+    def __init__(self,
+                 objective_fn,
+                 initilization_fn,
+                 data,
+                 optimizer,
+                 obj_threshold=1e-6,
+                 grad_threshold=1e-6,
+                 max_epochs=10_000):
         self.objective_fn = objective_fn
         self.initilization_fn = initilization_fn
         self.optimizer = optimizer
-        self.data = data
+        self.data = tuple([jnp.asarray(d) for d in data])
+        self.obj_threshold = obj_threshold
+        self.grad_threshold = grad_threshold
+        self.max_epochs = max_epochs
+
+    def __repr__(self):
+        pass
 
     @staticmethod
     def _collect_output(res):
-        return GD_Output(
+        bootstraped = res.converged.size > 1
+        return FitResults(
             theta=np.asarray(res.params),
-            converged=res.converged.item(),
-            step=res.step.item(),
-            objective_value=res.obj_keeper.item(),
+            converged=res.converged.item() if not bootstraped else np.asarray(res.converged),
+            convergence_epoch=res.convergence_epoch.item() if not bootstraped else np.asarray(res.convergence_epoch),
+            objective_value=res.obj_keeper.item() if not bootstraped else np.asarray(res.obj_keeper),
             grads=np.asarray(res.grads_keeper)
         )
 
     @staticmethod
-    def _clean_results(res):
-        idx_fittest = jnp.argmin(res.obj_keeper)
-        res = res.replace(
-            params=res.params[idx_fittest],
-            converged=res.converged[idx_fittest],
-            step=res.step[idx_fittest],
-            obj_keeper=res.obj_keeper[idx_fittest],
-            grads_keeper=res.grads_keeper[idx_fittest]
-        )
-        return res
+    def _clean_results(res, metrics):
+
+        bootstraped = res.converged.ndim > 1
+
+        if not bootstraped:
+
+            idx_fittest = jnp.argmin(res.obj_keeper)
+
+            res = res.replace(
+                params=res.params[idx_fittest],
+                converged=res.converged[idx_fittest],
+                convergence_epoch=res.convergence_epoch[idx_fittest],
+                step=res.step[idx_fittest],
+                obj_keeper=res.obj_keeper[idx_fittest],
+                grads_keeper=res.grads_keeper[idx_fittest],
+            )
+
+            metrics = {key: np.asarray(val[idx_fittest]) for (key, val) in metrics.items()}
+
+        else:
+
+            n_boot = res.converged.shape[0]
+            idx_fittest = jnp.argmin(res.obj_keeper, axis=1)
+
+            res = res.replace(
+                params=res.params[jnp.arange(n_boot), idx_fittest, :],
+                converged=res.converged[jnp.arange(n_boot), idx_fittest],
+                convergence_epoch=res.convergence_epoch[jnp.arange(n_boot), idx_fittest],
+                step=res.step[jnp.arange(n_boot), idx_fittest],
+                obj_keeper=res.obj_keeper[jnp.arange(n_boot), idx_fittest],
+                grads_keeper=res.grads_keeper[jnp.arange(n_boot), idx_fittest, :],
+                max_epochs=res.max_epochs[jnp.arange(n_boot), idx_fittest]
+            )
+
+            metrics = {key: np.asarray(val[idx_fittest, :]) for (key, val) in metrics.items()}
+
+        return res, metrics
 
     @partial(jax.jit, static_argnums=(0, ))
     def _initialize_theta(self, rng):
         return self.initilization_fn(rng)
 
+    @staticmethod
+    @partial(jax.jit)
+    def _get_convergence_epoch(training, epoch, converged):
+
+        old_converged, old_convergence_epoch = training.converged, training.convergence_epoch
+
+        return jnp.where(jnp.logical_and(old_converged, converged), old_convergence_epoch, epoch)
+
+
     @partial(jax.jit, static_argnums=(0,))
     def _check_convergence(self, training, obj_value, grads):
 
-        obj_old, grads_old = training.obj_keeper, training.grads_keeper
+        obj_old = training.obj_keeper
 
         obj_converged = jnp.less_equal(jnp.linalg.norm(obj_value - obj_old), self.obj_threshold)
-        grads_converged = jnp.less_equal(jnp.linalg.norm(grads - grads_old), self.grad_threshold)
 
-        return jnp.logical_or(obj_converged, grads_converged)
+        grads_converged = jnp.less_equal(jnp.linalg.norm(grads), self.grad_threshold)
+
+        converged = jnp.logical_or(obj_converged, grads_converged)
+
+        convergence_epoch = self._get_convergence_epoch(training, training.step, converged)
+
+        return converged, convergence_epoch
 
     @partial(jax.jit, static_argnums=(0,))
-    def _step(self, args):
+    def _step(self, args, epoch):
 
         training, data = args
 
         obj_value, grads = jax.jit(jax.value_and_grad(self.objective_fn, argnums=0, has_aux=False))(training.params, data)
 
-        converged = self._check_convergence(training, obj_value, grads)
+        converged, convergence_epoch = self._check_convergence(training, obj_value, grads)
 
         training = training.apply_gradients(grads=grads)
-        training = training.replace(obj_keeper=obj_value.squeeze(), grads_keeper=grads, converged=converged)
+        training = training.replace(
+            obj_keeper=obj_value.squeeze(),
+            grads_keeper=grads,
+            converged=converged,
+            convergence_epoch=convergence_epoch
+        )
 
-        return (training, data)
+        metrics = {
+            "objective_value": obj_value,
+            "epoch": epoch,
+            "converged": converged
+        }
 
-    @staticmethod
-    @partial(jax.jit)
-    def cond_fun(args):
-        steps_exceeded = jnp.greater_equal(args[0].step, args[0].max_epochs)
-        return jnp.logical_or(jnp.logical_not(args[0].converged), steps_exceeded)
+        return (training, data), metrics
 
     @partial(jax.jit, static_argnums=(0, ))
-    def _run_single(self, rng, max_epochs=20_000):
+    def _run_single(self, rng, data):
 
         theta_init = self._initialize_theta(rng)
 
@@ -95,67 +154,49 @@ class GradientDescent:
             apply_fn=self.objective_fn,
             params=theta_init,
             converged=jnp.array(False, dtype=jnp.bool_),
-            obj_keeper=jnp.array(9999, dtype=jnp.float32),
+            convergence_epoch=jnp.array(0, dtype=jnp.int32),
+            obj_keeper=jnp.array(0, dtype=jnp.float32),
             grads_keeper=jnp.ones_like(theta_init, dtype=jnp.float32) * 9999,
-            max_epochs=max_epochs,
             tx=self.optimizer
         )
 
-        runner = (training, self.data)
-        runner = lax.while_loop(self.cond_fun, self._step, runner)
+        step_runner = (training, data)
+        (training, data), metrics = lax.scan(scan_tqdm(self.max_epochs)(self._step), step_runner, jnp.arange(self.max_epochs), self.max_epochs)
 
+        return training, metrics
 
-        training, data = runner
-
-        return training
-
-    def _run(self, rng, n_inits=1, max_epochs=100_000, obj_threshold=1e-6, grad_threshold=1e-6):
-
-        self.obj_threshold = obj_threshold
-        self.grad_threshold = grad_threshold
+    def _run(self, rng, n_inits=1):
 
         if n_inits > 1:
             rngs = jax.random.split(rng, n_inits)
-            res = jax.jit(jax.vmap(self._run_single, in_axes=(0, None)))(rngs, max_epochs)
-            res = self._clean_results(res)
-            return self._collect_output(res)
+            res, metrics = jax.jit(jax.vmap(self._run_single, in_axes=(0, None)))(rngs, self.data)
+            res, metrics = self._clean_results(res, metrics)
+            return (self._collect_output(res), metrics)
         else:
-            res = jax.jit(self._run_single)(rng, max_epochs)
-            return self._collect_output(res)
+            res, metrics = jax.jit(self._run_single)(rng, self.data)
+            return (self._collect_output(res), metrics)
 
+    def _bootstrap(self, rng, n_inits=1, n_boot=1_000):
 
-if __name__ == "__main__":
+        rng, rng_boot = jax.random.split(rng)
 
-    from scipy import stats
+        idx_boot = jax.random.choice(
+            rng_boot,
+            jnp.arange(self.data[0].shape[0]),
+            shape=(n_boot, self.data[0].shape[0]),
+            replace=True
+        )
 
-    np.random.seed(42)
-    x = np.random.normal(size=100)
-    np.random.seed(32)
-    y = 3 + 2 * x + np.random.normal(size=100)
-    data = (np.c_[np.ones_like(x), x], y)
+        data_boot = tuple([jnp.take(d, idx_boot, axis=0) for d in self.data])
 
-    ols_slope, ols_intercept, _, _, _ = stats.linregress(x, y)
-
-    def objective_fn(theta, data):
-        yhat = data[0].dot(theta)
-        y = data[1]
-        loss = jnp.sum((y - yhat)**2) / y.size
-        return loss
-
-    def initilization_fn(rng):
-        # return jax.random.normal(rng, shape=(rng.shape[0], ))
-        return jax.random.normal(rng, shape=(2, ))
-
-    # optimizer = optax.adam(learning_rate=1e-3, eps=1e-5)
-    scheduler = optax.exponential_decay(init_value=1e-3, transition_steps=1000, decay_rate=0.99)
-    optimizer = optax.chain(
-        optax.clip_by_global_norm(1.0),  # Clip by the gradient by the global norm.
-        optax.scale_by_adam(),  # Use the updates from adam.
-        optax.scale_by_schedule(scheduler),  # Use the learning rate from the scheduler.
-        optax.scale(-1.0) # Scale updates by -1 since optax.apply_updates is additive and we want to descend on the loss.
-    )
-
-    rng = jax.random.PRNGKey(42)
-    gd = GradientDescent(objective_fn, initilization_fn, data, optimizer)
-    res = gd._run(rng, n_inits=5, obj_threshold=1e-10, grad_threshold=1e-10, max_epochs=10_000)
+        if n_inits > 1:
+            rngs = jax.random.split(rng, n_inits)
+            res, metrics = jax.jit(jax.vmap(
+                jax.vmap(self._run_single, in_axes=(0, None)),
+                in_axes=(None, 0)))(rngs, data_boot)
+            res, metrics = self._clean_results(res, metrics)
+            return (self._collect_output(res), metrics)
+        else:
+            res, metrics = jax.jit(jax.vmap(self._run_single, in_axes=(None, 0)))(rng, data_boot)
+            return (self._collect_output(res), metrics)
 
